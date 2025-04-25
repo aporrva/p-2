73f85becce001097cbc0cf3fe649df45d04b14ac
aporrva <a9296430@gmail.com>
2025-04-25 22:46:26 +0530

    y

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,9 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
+        <h1 className=''>
+          apoorva
+        </h1>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
